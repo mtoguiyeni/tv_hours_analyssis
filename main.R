2baffaea4e3bdd394b3d99ea23317ba054186648
mtoguiyeni <mtoguiyeni@gmail.com>
2024-04-15 14:51:51 +0000

    started analysis

---
+++ main.R
@@ -0,0 +1,11 @@
+
+# exemple
+
+library(tidyverse)
+library(here)
+
+tv_hours_table <- gss_cat %>%
+  group_by(marital) %>%
+  summarise(mean_tv_hours = mean(tvhours, na.rm = T))
+
+write_csv(tv_hours_table, here("Tv_hours_table_martital.csv"))
